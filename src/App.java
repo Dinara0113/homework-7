f1afa5167b9a1048653893ee0216011d0ba7b806
Пользователь <dinarkatttb16@gmail.com>
2024-05-05 23:32:37 +0500

    objects 1 done

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@
 public class App {
     public static void main(String[] args) {
         Author rend = new Author("Ayn", "Rend");
-        Author sharlotta = new Author("Jeyn", "Eyr");
+        Author sharlotta = new Author("Sharlotta", "Bronte");
 
         Book book = new Book("Atlant Raspravil Plechi", rend, 1974);
         Book book2 = new Book("Jeyn Eyr", sharlotta, 2020);
